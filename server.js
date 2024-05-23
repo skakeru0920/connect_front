edb95137a6dc14f97dfcbc50177e7e6f52bc31e4
Kakeru Sano <skakeru0920@gmail.com>
2024-05-24 01:15:21 +0900

    add seed data

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
-
 const app = express();
+
+const list = [
+	'翻訳アプリをダウンロードする',
+	'戸締りをする',
+	'猫を抱きしめる',
+	'荷造りをする',
+	' 航空券を買う',
+	'もう一度猫を抱きしめる',
+];
+
+app.get('/api/list', (req, res) => {
+	res.json(list);
+});
 app.listen(3000, () => {
 	console.log('Server running on http://localhost:3000');
 });
