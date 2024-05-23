8fc1c0190b96da2b9c0b2bfb3db3fdfbf4235f51
Kakeru Sano <skakeru0920@gmail.com>
2024-05-24 01:25:25 +0900

    crete list view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
+	const [list, setList] = useState([
+		'翻訳アプリをダウンロードする',
+		'戸締りをする',
+		'猫を抱きしめる',
+		'荷造りをする',
+		'航空券を買う',
+		'もう一度猫を抱きしめる',
+	]);
 
-  return (
-    <>
-      <h1>ToDoリスト</h1>
-    </>
-  )
+	return (
+		<>
+			<h1>ToDoリスト</h1>
+			<input />
+			<ul>
+				{list.map((task, i) => (
+					<li key={i}>{task}</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
-export default App
+export default App;
